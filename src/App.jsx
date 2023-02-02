f19916c2aae3041bd6c9703209111fccac320508
Bekhzod Akbarov <stx.webdev@gmail.com>
2023-02-02 22:26:47 +0500

    remove react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import About from './pages/About';
@@ -11,31 +10,26 @@ import { AlertProvider } from './context/alert/AlertContext';
 import Alert from './components/layout/Alert';
 import User from './pages/User';
 
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <GithubProvider>
-        <AlertProvider>
-          <Router>
-            <div className='flex flex-col justify-between h-screen'>
-              <Navbar />
-              <main className='container mx-auto px-3 pb-12'>
-                <Alert />
-                <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/about' element={<About />} />
-                  <Route path='/user/:username' element={<User />} />
-                  <Route path='/*' element={<NotFound />} />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
-          </Router>
-        </AlertProvider>
-      </GithubProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:username' element={<User />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
