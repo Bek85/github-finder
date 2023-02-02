db3898e6fe3b5f4c99957f6ae936939d4fcd1eb8
Bekhzod Akbarov <stx.webdev@gmail.com>
2023-02-02 11:19:04 +0500

    build <UserSearch/> component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserList from '../components/users/UserList';
+import UserSearch from '../components/users/UserSearch';
 
 export default function Home() {
   return (
     <>
-      {/* Search Component goes here */}
+      <UserSearch />
       <UserList />
     </>
   );
